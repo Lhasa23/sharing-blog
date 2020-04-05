c13a54139bfd3bfe9dc617b306fcc2307efeb5a8
eleven-shi <sydryy@gmail.com>
2020-04-05 22:10:49 +0800

    create pages

---
+++ src/pages/Logup/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>注册</h2>
+  </div>
+</template>
+
+<script src="./index.js"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src="./index.css"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/Home/index.vue'
+import Logup from '@/pages/Logup/index.vue'
+import Login from '@/pages/Login/index.vue'
+import Create from '@/pages/Create/index.vue'
+import Detail from '@/pages/Detail/index.vue'
+import Edit from '@/pages/Edit/index.vue'
+import My from '@/pages/My/index.vue'
+import User from '@/pages/User/index.vue'
 
 Vue.use(Router)
 
@@ -8,8 +15,36 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    }, {
+      path: '/logup',
+      name: 'Logup',
+      component: Logup
+    }, {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }, {
+      path: '/create',
+      name: 'Create',
+      component: Create
+    }, {
+      path: '/detail',
+      name: 'Detail',
+      component: Detail
+    }, {
+      path: '/edit',
+      name: 'Edit',
+      component: Edit
+    }, {
+      path: '/my',
+      name: 'My',
+      component: My
+    }, {
+      path: '/user',
+      name: 'User',
+      component: User
     }
   ]
 })
